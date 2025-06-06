f2ff1a868b6431d9d7c5de4efd4b04b7012fc0ff
rm-reins <rm_reins@icloud.com>
2025-06-06 03:39:18 +0200

    feat: update HomeScreen with layout, balance display; add mock data

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,15 @@
-import { Text, View } from "react-native";
+import { GlobalStyles } from "@/constants/styles";
+import HomeScreen from "@/screens/HomeScreen";
+import { StatusBar, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={[GlobalStyles.screenContainer]}>
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+      />
+      <HomeScreen />
     </View>
   );
 }
