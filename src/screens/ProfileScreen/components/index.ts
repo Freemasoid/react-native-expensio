227f627428b735cd8a1ef427156944567e48c31e
rm-reins <rm_reins@icloud.com>
2025-06-12 18:48:30 +0200

    feat: add user info, statistics and settings to profile screen

---
+++ src/screens/ProfileScreen/components/index.ts
@@ -0,0 +1,4 @@
+export { SettingsSection } from "./SettingsSection";
+export { StatsCard } from "./StatsCard";
+export { ThemeSwitcher } from "./ThemeSwitcher";
+export { UserCard } from "./UserCard";
