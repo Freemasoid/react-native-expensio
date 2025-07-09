1101a56b22301c776e2361f1d63e9d7f855a628c
Freemasoid <freemasoid@icloud.com>
2025-07-09 15:50:25 +0200

    refactor: remove StatsCard component and update ProfileScreen imports

--- src/screens/ProfileScreen/components/index.ts
+++ src/screens/ProfileScreen/components/index.ts
@@ -1,4 +1,3 @@
 export { SettingsSection } from "./SettingsSection";
-export { StatsCard } from "./StatsCard";
 export { ThemeSwitcher } from "./ThemeSwitcher";
 export { UserCard } from "./UserCard";
